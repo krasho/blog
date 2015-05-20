1f33d6ef8a7b55863903cc9777fed7773474c9e9
Krasho <joseluis@balidevelop.com>
2015-05-20 00:16:44 -0400

    category model created

--- spec/requests/categories_pages_spec.rb
+++ spec/requests/categories_pages_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "CategoriesPosts", type: :request do
+  let!(:categories) {FactoryGirl.create_list(:category,5)}
+
+  describe "Listing All Categries" do 
+     before do
+     	visit categories_path
+     end
+
+	context "Listing all categories" do 
+		it "Should list all available categories data" do 
+			categories.each do |category|
+				expect(page).to have_content category.name
+			end
+		end
+	end
+
+  end
+
 end

a1ab28d4ed033e28be2e2a369eb990bca0c233c1
Jose Luis <tigrillo34@hotmail.com>
2015-05-20 17:08:33 -0400

    Adding editorconfig

--- spec/requests/categories_pages_spec.rb
+++ spec/requests/categories_pages_spec.rb
@@ -6,10 +6,14 @@ RSpec.describe "CategoriesPosts", type: :request do
   describe "Listing All Categries" do 
      before do
      	visit categories_path
-     	save_and_open_page
      end
 
 	context "Listing all categories" do 
+        it "Should be title Listado de Categorías" do 
+        	expect(page).to have_content "Listado de Categorías"
+        end
+
+
 		it "Should list all available categories data" do 
 			categories.each do |category|
 				expect(page).to have_content category.name
